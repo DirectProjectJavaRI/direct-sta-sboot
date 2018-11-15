2e5e387b6556690f4c0c37afb34291e8e9ab0aa5
gm2552 <gm2552@localhost>
2018-11-15 14:42:00 -0600

    Adding xm components to configuration class.

--- src/main/java/org/nhindirect/stagent/boot/STAApplication.java
+++ src/main/java/org/nhindirect/stagent/boot/STAApplication.java
@@ -2,19 +2,32 @@ package org.nhindirect.stagent.boot;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan(basePackages = {"org.nhindirect.gateway.springconfig", "org.nhindirect.gateway.streams"})
+@ComponentScan(basePackages = {"org.nhindirect.gateway.springconfig", "org.nhindirect.gateway.streams", "org.nhind.xdm.springconfig"})
 @SpringBootApplication
 @Configuration
 @EnableEurekaClient
-public class STAApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class STAApplication extends SpringBootServletInitializer
 {
     public static void main(String[] args) 
     {
-        SpringApplication.run(STAApplication.class, args);
+    	SpringApplication.run(STAApplication.class, args);
     }  
+    
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) 
+	{
+	    return application.sources(STAApplication.class);
+	}	    
 }
